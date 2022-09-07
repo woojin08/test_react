7fb72a3ca8cb137cae587281a72bc5b3f1070430
kimwoojin <kimwoojin08@naver.com>
2022-09-07 16:10:26 +0900

    20220907

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
+import { useState } from 'react';
 import './App.css';
 
+const DB = [
+  {
+    id: 1,
+    content: "menu01",
+    link: "/s1",
+    submenu: [
+      { content: "smenu011", link: "/1" },
+      { content: "smenu012", link: "/2" },
+      { content: "smenu013", link: "/3" },
+
+    ]
+  },
+  {
+    id: 2,
+    content: "menu02",
+    link: "/s1",
+    submenu: [
+      { content: "smenu021", link: "/1" },
+      { content: "smenu022", link: "/2" },
+      { content: "smenu023", link: "/3" },
+
+    ]
+  },
+  {
+    id: 3,
+    content: "menu03",
+    link: "/s1",
+    submenu: [
+      { content: "smenu031", link: "/1" },
+      { content: "smenu032", link: "/2" },
+      { content: "smenu033", link: "/3" },
+
+    ]
+  }
+]
+
 function App() {
+  const [CB, setCB] = useState("");
+  const [TG, setTG] = useState(false);
+  // const [TEXT, setTEXT] = useState(false);
   return (
-    <>
-      README
-    </>
+    <header>
+      <h1 className={CB} onClick={() => setCB('on')}>LOGO</h1>
+      <h2 className={`ggg ${TG ? 'on' : ''}`}>toggle class</h2>
+      <button onClick={() => setTG(!TG)}>class 토글</button>
+      {/* <button onClick={() => setTEXT(!TEXT)}>글자겹치는 거 나오는 버튼</button> */}
+      <nav className='GNB'>
+        <ul>
+          {
+            DB.map((it, idx) => <li key={idx}>
+              <a href={it.link}>{it.content}</a>
+              <ul className='smenu'>
+                {
+                  it.submenu.map((smenu, idx) => <li key={idx}>
+                    <a href={smenu.link}>{smenu.content}</a>
+                  </li>)
+                }
+              </ul>
+            </li>)
+          }
+        </ul>
+      </nav>
+      {/* {
+        TEXT && <DoubleText />
+      } */}
+
+    </header>
+
   );
 }
 
